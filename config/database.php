29323c506efe3329bc58bff36b0c010bef813dcd
Your Name <shohan123boss@gmail.com>
2022-02-09 20:27:45 +0600

    Update database connection

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('DATABASE_URL');
+
 return [
 
     /*
@@ -15,7 +17,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +69,21 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            
+            
+            //'host' => env('DB_HOST', '127.0.0.1'),
+                      
+ 
+
+            'host' =>$DATABASE_URL['host'], 
+            'port' =>$DATABASE_URL['port'], 
+            'database' =>ltrim($DATABASE_URL ["path"], charlist:"/"),
+            'username' => $DATABASE_URL ["user"],
+            'password' =>  $DATABASE_URL ["pass"],
+           // 'port' => env('DB_PORT', '5432'),
+           // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -92,6 +105,11 @@ return [
         ],
 
     ],
+
+
+
+   
+    
 
     /*
     |--------------------------------------------------------------------------
